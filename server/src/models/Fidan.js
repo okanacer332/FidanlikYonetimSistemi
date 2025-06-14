9e9043768db4995c2b9419579db8ece1ea1032de
okanacer332 <acer.okanumut@hotmail.com>
2025-06-14 14:55:23 +0300

    Update Fidan model fields

--- server/src/models/Fidan.js
+++ server/src/models/Fidan.js
@@ -1,29 +1,44 @@
 // server/src/models/Fidan.js
 const { Schema, model } = require('mongoose'); //
 
-const fidanSchema = new Schema({ //
-  name: { //
-    type: String, //
-    required: true, //
+const fidanSchema = new Schema({
+  ad: {
+    type: String,
+    required: true,
+    trim: true,
   },
-  parentId: { //
-    type: Schema.Types.ObjectId, //
-    ref: 'Fidan', //
-    default: null, //
+  aciklama: {
+    type: String,
+    default: '',
   },
-  musteriId: { // <-- EKLENEN KRİTİK ALAN //
-    type: String, //
-    required: true, //
+  stokMiktari: {
+    type: Number,
+    required: true,
+    min: 0,
   },
-  // fidanKodu: { // Kaldırıldı
-  //   type: String,
-  //   unique: true,
-  //   sparse: true,
-  // },
-  // satisFiyati: { // Kaldırıldı
-  //   type: Number,
-  // },
-}, { timestamps: true }); //
+  alisFiyati: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  satisFiyati: {
+    type: Number,
+    default: 0,
+    min: 0,
+  },
+  kategori: {
+    type: String,
+    default: '',
+  },
+  tedarikci: {
+    type: String,
+    default: '',
+  },
+  lokasyon: {
+    type: String,
+    default: '',
+  },
+}, { timestamps: true });
 
 const Fidan = model('Fidan', fidanSchema); //
 module.exports = Fidan;
