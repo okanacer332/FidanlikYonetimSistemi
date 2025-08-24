0023287c0df9df2d8775c3b4c04cb830451865da
okanacer332 <acer.okanumut@hotmail.com>
2025-08-24 13:04:55 +0300

    update

--- fidanys-server/src/main/java/com/fidanlik/fidanysserver/role/repository/RoleRepository.java
+++ fidanys-server/src/main/java/com/fidanlik/fidanysserver/role/repository/RoleRepository.java
@@ -1,10 +1,13 @@
-// Yeni konum: src/main/java/com/fidanlik/fidanysserver/role/repository/RoleRepository.java
 package com.fidanlik.fidanysserver.role.repository;
 
-import com.fidanlik.fidanysserver.role.model.Role; // Yeni paket yolu
+import com.fidanlik.fidanysserver.role.model.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+import java.util.Optional; // Optional import'unu ekleyin
 
 public interface RoleRepository extends MongoRepository<Role, String> {
     List<Role> findAllByTenantId(String tenantId);
+
+    // YENİ EKLENEN METOT: İsim ve Tenant ID'ye göre bir rol bulur.
+    Optional<Role> findByNameAndTenantId(String name, String tenantId);
 }
